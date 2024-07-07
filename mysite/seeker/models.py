fdce9c38a58790434a0a881529e7e63c102ccc75
Hankiii890 <shenderuk4kir@gmail.com>
2024-07-07 20:15:23 +0300

    problem db

--- mysite/seeker/models.py
+++ mysite/seeker/models.py
@@ -30,3 +30,6 @@ class Message(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Проверка {self.executor}'  # Не работает
