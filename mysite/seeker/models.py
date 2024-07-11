99cc39fa31f626cf060b74c7a8b2a26ac30c52ed
Hankiii890 <shenderuk4kir@gmail.com>
2024-07-11 22:49:22 +0300

    re-creation

--- mysite/seeker/models.py
+++ mysite/seeker/models.py
@@ -6,28 +6,37 @@ from django.utils import timezone
 
 class Executor(models.Model):
     """Исполнитель"""
-    name = models.CharField(max_length=50)
-    skills = models.ManyToManyField('Skill', related_name='executors')
-    experience = models.IntegerField()
-    education = models.TextField()
-    phone = models.CharField(max_length=12)
-    email = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    skills = models.ManyToManyField('Skill', related_name='executors', verbose_name='Навыки')
+    experience = models.IntegerField(verbose_name='Опыт')
+    education = models.TextField(verbose_name='Образование')
+    phone = models.CharField(blank=True, verbose_name='Телефон')
+    email = models.EmailField(unique=True, verbose_name='Email')
 
+    class Meta:
+        verbose_name = 'Исполнитель'
+        verbose_name_plural = 'Исполнители'
 
 class Skill(models.Model):
     """Навыки"""
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, verbose_name='Описание')
 
     def __str__(self):
         return self.description
 
+    class Meta:
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
 
 class Message(models.Model):
     """ Model for message between executor and user  """
 
-    executor = models.ForeignKey('Executor', on_delete=models.CASCADE, related_name='messages')     # Можем получить список всех сообщение исполнителя с помощью: executor.messages
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
+    executor = models.ForeignKey('Executor', on_delete=models.CASCADE, related_name='messages', verbose_name='Исполнитель')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_read = models.BooleanField(default=False, verbose_name='Прочитано')
 
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
