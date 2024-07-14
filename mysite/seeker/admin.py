0fadd66b5f7dee748f8bb8842a9891b6730f2498
Hankiii890 <shenderuk4kir@gmail.com>
2024-07-14 23:48:03 +0300

    Cleaning

--- mysite/seeker/admin.py
+++ mysite/seeker/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-
 from .models import Executor, Skill, Message
-
-# Register your models here.
-
 
 admin.site.register(Executor)
 admin.site.register(Skill)
--- mysite/seeker/models.py
+++ mysite/seeker/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.apps import apps
 from django.conf import settings
-from django.utils import timezone
 
 
 class Executor(models.Model):
